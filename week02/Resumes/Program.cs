3090ffd71c7d7ae7ef73e9516bc968b6618ca977
Hernan-CS <hcancinosantiago@byupathway.edu>
2025-05-15 02:19:09 -0500

    W02 Learning Activity: Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Console.WriteLine("---Résumé---");
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Junior Programmer";
+        job1._startYear = 2013;
+        job1._endYear = 2017;
+
+        Job job2 = new Job();
+        job2._company = "Mojang";
+        job2._jobTitle = "Designer";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "John Doe";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
