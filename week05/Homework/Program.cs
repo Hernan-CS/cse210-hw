24020954993cf70ef17f9920d8345d5920860759
Hernan-CS <hcancinosantiago@byupathway.edu>
2025-06-03 22:52:02 -0500

    W05 Learning Activity: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Hernan Cancino", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment m1 = new MathAssignment("Hernan Cancino", "Multiplication", "7", "0-100");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment w1 = new WritingAssignment("Magda Santiago", "Peruvian History", "Black Death in Peru - 19030");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
     }
 }
